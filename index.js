a2da1bd1c1364020340fce0cc2979dc22f66b9d1
liu-kat <katherineliu25@gmail.com>
2024-04-09 16:00:23 -0700

    codeeee

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $(".hero-slider").slick({
+    autoplay: true,
+    autoplaySpeed: 2500, // Adjust speed as needed
+    arrows: false, // Remove navigation arrows if not needed
+    dots: true, // Add navigation dots
+  });
+});
